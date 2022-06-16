71433e8d8c67b45b7d6da48ab3e26545394ce3db
Coder-Matheo <ali.dinarvand1370@gmail.com>
2022-06-16 20:59:59 +0200

    colored selected item

--- app/src/main/java/rob/myappcompany/recyclerviewusetablayoutfragmentdemo/RecyclerViewAdapter.java
+++ app/src/main/java/rob/myappcompany/recyclerviewusetablayoutfragmentdemo/RecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package rob.myappcompany.recyclerviewusetablayoutfragmentdemo;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +19,7 @@ import java.util.List;
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
     Context mConact;
     List<Conact> mData;
+    int selected_position = 0; // You have to set this globally in the Adapter class
 
 
     public RecyclerViewAdapter(Context mConact, List<Conact> mData) {
@@ -30,12 +32,10 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view;
         view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact, parent, false);
-        MyViewHolder viewHolder = new MyViewHolder(view);
 
 
 
-
-        return viewHolder;
+        return new MyViewHolder(view);
     }
 
     @Override
@@ -43,6 +43,10 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.tv_name.setText(mData.get(position).getName());
         holder.tv_phone.setText(mData.get(position).getPhote());
         holder.img.setImageResource(mData.get(position).getPhote());
+
+
+        // Here I am just highlighting the background
+        holder.itemView.setBackgroundColor(selected_position == position ? Color.GREEN : Color.TRANSPARENT);
     }
 
 
@@ -52,7 +56,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return mData.size();
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private LinearLayout item_conact;
         private TextView tv_name;
         private TextView tv_phone;
@@ -62,6 +66,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            itemView.setOnClickListener(this);
+
             item_conact = itemView.findViewById(R.id.conact_item_id);
             tv_name = itemView.findViewById(R.id.name_contact);
             tv_phone = itemView.findViewById(R.id.phone_conact);
@@ -73,6 +80,21 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                     Log.i("TAG", "onClick: "+ tv_name.getText());
                 }
             });
+
+
+        }
+        @Override
+        public void onClick(View v) {
+            // Below line is just like a safety check, because sometimes holder could be null,
+            // in that case, getAdapterPosition() will return RecyclerView.NO_POSITION
+            if (getAdapterPosition() == RecyclerView.NO_POSITION) return;
+
+            // Updating old as well as new positions
+            notifyItemChanged(selected_position);
+            selected_position = getAdapterPosition();
+            notifyItemChanged(selected_position);
+
+            // Do your another stuff for your onClick
         }
 
 
